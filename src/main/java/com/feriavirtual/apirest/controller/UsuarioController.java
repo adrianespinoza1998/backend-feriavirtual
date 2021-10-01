14292c75c40dfcbc18d3a250310f32c0df4845d8
Adrian Espinoza <adrianespinozaarevalo@gmail.com>
2021-10-01 18:52:56 -0300

    add cors

--- src/main/java/com/feriavirtual/apirest/controller/UsuarioController.java
+++ src/main/java/com/feriavirtual/apirest/controller/UsuarioController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +19,7 @@ import com.feriavirtual.apirest.models.Usuario;
 import com.feriavirtual.apirest.service.IUsuarioService;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 public class UsuarioController {
 
 	@Autowired
