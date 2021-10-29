c4fab26189d373af311017c7c5b852354fcd9843
Adrian Espinoza <adrianespinozaarevalo@gmail.com>
2021-10-29 13:32:37 -0300

    fix start project

--- src/main/java/com/feriavirtual/apirest/PruebaApirestApplication.java
+++ src/main/java/com/feriavirtual/apirest/PruebaApirestApplication.java
@@ -2,8 +2,12 @@ package com.feriavirtual.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Configuration
 @SpringBootApplication
+@EnableScheduling
 public class PruebaApirestApplication {
 
 	public static void main(String[] args) {
